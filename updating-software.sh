78b4ea128ab37312795da00655b15f09fca240ce
Luis Puerto <luis@luispuerto.net>
2019-07-22 15:57:15 -0700

    disable update python packages on up-all

--- updating-software.sh
+++ updating-software.sh
@@ -48,7 +48,7 @@ function up-all () {
 	up-r-packages
 	echo "\U1F4CC ${RED}==>${NC} Updating Ruby \U1F48E gems \U1F91E"
 	gem update 
-	up-py-packages
+	# up-py-packages
 	echo "\U1F4CC ${RED}==>${NC} updating macOS Apps in the App Mac Store \U1F5A5 \U1F91E"
 	mas upgrade
 	echo "\U1F4CC ${RED}==>${NC} updating macOS System \U1F5A5 \U1F91E"
