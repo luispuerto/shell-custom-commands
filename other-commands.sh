fe63eab0820cc682be8c9cd4abad463cc928904d
Luis Puerto <luis@luispuerto.net>
2022-11-27 17:49:17 +0100

    Add pdfcompress function

--- other-commands.sh
+++ other-commands.sh
@@ -63,3 +63,24 @@ function pcre2-libs-ln () {
                /opt/homebrew/opt/pcre2/lib/libpcre2-posix.dylib \
    /opt/R/arm64/lib/
 }
+
+
+pdfcompress (){
+  # This is coming from here: https://gist.github.com/ahmed-musallam/27de7d7c5ac68ecbd1ed65b6b48416f9
+  gs \
+ -q -dNOPAUSE -dBATCH -dSAFER \
+ -sDEVICE=pdfwrite \
+ -dCompatibilityLevel=1.4 \
+ -dPDFSETTINGS=/screen \
+ -dEmbedAllFonts=true -dSubsetFonts=true \
+ -dColorImageDownsampleType=/Bicubic \
+ -dColorImageResolution=144 \
+ -dGrayImageDownsampleType=/Bicubic \
+ -dGrayImageResolution=144 \
+ -dMonoImageDownsampleType=/Subsample \
+ -dMonoImageResolution=144 \
+ -dNOTRANSPARENCY \
+ -sOutputFile=${1%.*}.compressed.pdf \
+ $1;
+}
+
